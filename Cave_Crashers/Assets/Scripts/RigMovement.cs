dd444e0f120e5d7b04739441343f8309d01f1693
BeefNBrocoli <atran6734@gmail.com>
2025-11-03 09:49:50 -0500

    (Barely) Started trying to prototype Rig Movement

---
+++ Cave_Crashers/Assets/Scripts/RigMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class RigMovement : MonoBehaviour, IInteractable
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        //Set Players Transform to Pilot Seat Transform
+
+        //UI Transition?
+
+        //Movement Controls
+
+ 
+    }
+
+}
